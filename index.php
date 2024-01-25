8f685cc469955be0be2ee3f4e38d3f52b146b308
gds90 <you@example.com>
2024-01-25 14:56:49 +0100

    Moved hotel infos in a bootstrap table

--- index.php
+++ index.php
@@ -36,20 +36,68 @@ $hotels = [
         'distance_to_center' => 50
     ]
 ];
+?>
 
-foreach ($hotels as $hotel) {
-    echo "Nome: " . ($hotel['name']) . " ";
-    echo "<br>";
-    echo "Descrizione: " . ($hotel['description']) . " ";
-    echo "<br>";
-    if ($hotel['parking']) {
-        echo "Parcheggio disponibile: Sì ";
-    } else {
-        echo "Parcheggio disponibile: No ";
-    }
-    echo "<br>";
-    echo "Voto: " . ($hotel['vote']) . " ";
-    echo "<br>";
-    echo "Distanza dal centro: " . ($hotel['distance_to_center']) . "km";
-    echo "<br><br>";
-}
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <title>Hotel</title>
+</head>
+
+<body>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="my-2">Hotel della zona</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <table class="table table-dark table-striped">
+                        <thead>
+                            <tr>
+                                <th>Nome</th>
+                                <th>Descrizione</th>
+                                <th>Parcheggio</th>
+                                <th>Voto</th>
+                                <th>Distanza dal centro</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($hotels as $hotel) { ?>
+                                <tr>
+                                    <td>
+                                        <?php echo $hotel['name'] ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $hotel['description'] ?>
+                                    </td>
+                                    <td>
+                                        <?php if ($hotel['parking']) {
+                                            echo "Parcheggio disponibile: Sì ";
+                                        } else {
+                                            echo "Parcheggio disponibile: No ";
+                                        } ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $hotel['vote'] ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $hotel['distance_to_center'] . " km" ?>
+                                    </td>
+                                </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
